06ae7dd4a6244d36bd52f42dc8bab4751836c460
5y5tem5 <5y5tem5@localhost>
2022-10-23 18:03:03 -0500

    checkin

---
+++ signmodules.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+secure_crypto_dir="/private/"
+if (($EUID != 0)); then
+    echo "Must be run with root privs"
+    exit
+fi
+#Getting the sig_key for vmnet
+VMNetSigner="$(modinfo -F sig_key vmnet |  tr '[:upper:]' '[:lower:]')"
+
+#Getting the sig_key for vmmon
+VMMonSigner="$(modinfo -F sig_key vmmon |  tr '[:upper:]' '[:lower:]')"
+
+#Checking if vmmon key is trusted
+mokutil -l | grep -w "$VMMonSigner"
+VMMonSigned=$?
+
+#Checking if vmnet key is trusted
+mokutil -l | grep -w "$VMNetSigner"
+VMNetSigned=$?
+
+#if both are signed by trusted key we are good and can exit
+if [ $VMMonSigned -eq 0 ] && [ $VMNetSigned -eq 0 ]; then
+    echo "Both modules are signed by a trusted MOK Key"
+    echo "Leaving"
+    exit
+    
+fi
+
+#install modules 
+
+vmware-modconfig --console --install-all
+
+#check if dir exists and create it if not 
+if ([ ! -d $secure_crypto_dir ]); then
+    mkdir $secure_crypto_dir
+fi
+cd $secure_crypto_dir
+
+#generate certificate (this is lazy and should be fixed to something trustworthy ) 
+openssl req -new -x509 -newkey rsa:2048 -keyout MOK.priv -outform DER -out MOK.der -nodes -days 36500 -subj "/CN=VMware/"
+
+#Import certificate 
+mokutil --import MOK.der
+#sign the modules
+/usr/src/linux-headers-$(uname -r)/scripts/sign-file sha256 ./MOK.priv ./MOK.der $(modinfo -n vmmon)
+/usr/src/linux-headers-$(uname -r)/scripts/sign-file sha256 ./MOK.priv ./MOK.der $(modinfo -n vmnet)
